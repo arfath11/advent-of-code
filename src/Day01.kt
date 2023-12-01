477811beeaad99e79ac061497e674591bbaf51ee
arfath <md8arfath@gmail.com>
2023-12-01 20:06:54 +0530

    day one task  part 1 one

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,7 +1,43 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun findFirsrtdig(word: String): Int {
+        var ans = 0
+        for(ch in word)
+            if(ch.isDigit())
+            {
+                ans = ch.code - '0'.code
+                break
+            }
+        return ans
+
     }
+
+
+    fun part1(input: List<String>): Int {
+        var ans = 0
+        for(word in input){
+            var digCheck = false
+            for(ch in word ){
+                if(ch.isDigit())
+                    digCheck = true
+            }
+
+            if(!digCheck)
+                continue
+            var frsDig =    findFirsrtdig(word)
+            var lastdig = findFirsrtdig(word.reversed())
+            ans += (frsDig * 10 + lastdig)
+
+
+
+
+
+        }
+        return ans
+    }
+
+
+
 
     fun part2(input: List<String>): Int {
         return input.size
@@ -9,11 +45,11 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
 
-    val testInput = readInput("Day01_test")
-    print(part1(testInput))
-    check(part1(testInput) == 142)
+//    val testInput = readInput("Day01_test")
+//    print(part1(testInput))
+//    check(part1(testInput) == 142)
 
-//    val input = readInput("Day01")
-//    part1(input).println()
+    val input = readInput("Day01")
+    part1(input).println()
 //    part2(input).println()
 }
