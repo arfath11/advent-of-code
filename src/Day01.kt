8d9ede8d33db70f9abad0f452051e78a3ebf9af4
arfath <md8arfath@gmail.com>
2023-12-01 19:17:14 +0530

    setup done

--- src/Day01.kt
+++ src/Day01.kt
@@ -9,9 +9,10 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    print(part1(testInput))
+    check(part1(testInput) == 142)
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+//    val input = readInput("Day01")
+//    part1(input).println()
+//    part2(input).println()
 }
